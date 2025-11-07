857da17ea4c5fc57f460d80f4129210bc6faf3a5
alpha <142711552+duyAnNek@users.noreply.github.com>
2025-11-07 11:34:46 +0700

    Delete client directory

--- client/src/services/chat.service.js
+++
@@ -1,7 +0,0 @@
-import { api } from '../api/axios';
-export const chatService = {
-  async send(message) {
-    const { data } = await api.post('/chat', { message });
-    return data; // { reply }
-  }
-};
--- client/src/services/data.service.js
+++
@@ -1,8 +0,0 @@
-// client/src/services/data.service.js
-import api from '../api/axios';
-
-export const dataService = {
-  getEmployees: () => api.get('/employees').then(r => r.data),            
-  getProducts: () => api.get('/products').then(r => r.data),              
-  getInventoryValue: () => api.get('/inventory/value').then(r => r.data), 
-};
